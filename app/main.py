9c191565b0060d11d410a8ac829e7d732c01d856
kdnth <kadenconradt@gmail.com>
2026-07-21 15:06:47 -0400

    feat: creates and registers user router for fetching current user details

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from app.api.routes import auth, portfolio
+from app.api.routes import auth, portfolio, user
 
 app = FastAPI(title="Portfolio Tracker")
 
 app.include_router(auth.router)
 app.include_router(portfolio.router)
+app.include_router(user.router)
