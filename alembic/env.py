5f3cb5c4e5df1b4ecdba97c32b407f889187da9d
kdnth <kadenconradt@gmail.com>
2026-07-21 16:52:40 -0400

    fix: alembic online migrations ignoring settings DATABASE_URL

--- alembic/env.py
+++ alembic/env.py
@@ -58,14 +58,11 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+    configuration = config.get_section(config.config_ini_section, {})
+    configuration["sqlalchemy.url"] = settings.database_url
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
@@ -77,7 +74,6 @@ def run_migrations_online() -> None:
 
         with context.begin_transaction():
             context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
